d297d7cfc134287f94cdd3def3c9fcbb586311da
RubenGH88 <104738166+RubenGH88@users.noreply.github.com>
2022-06-21 15:15:53 +0200

    lab-intro-dom
    lab done

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items=[]
+    this.length=this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length=this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]){throw new Error('OutOfBounds')}
+    else return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(!this.length){throw new Error('EmptySortedList')}
+    else return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if(!this.length){throw new Error('EmptySortedList')}
+    else return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((total, number) => total + number, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) {
+			throw new Error('EmptySortedList');
+		}
+
+		return this.sum() / this.length;
+	
+  }
 }
 
 module.exports = SortedList;
